11adf067e60f2b9d59fbc3ef588a92eae6c56362
aaron <zhengdongquan@hotmail.com>
2015-11-26 14:37:06 -0500

    add http call

--- example/shoe/server.js
+++ example/shoe/server.js
@@ -6,11 +6,29 @@ var dnode = require('dnode');
 var server = http.createServer(ecstatic);
 server.listen(9999);
 
+var options = {
+  host: 'sinorama.ca',
+  path: '/'
+};
+
 var sock = shoe(function (stream) {
     var d = dnode({
         transform : function (s, cb) {
-            var res = s.replace(/[aeiou]{2,}/, 'oo').toUpperCase();
-            cb(res);
+            //var res = s.replace(/[aeiou]{2,}/, 'oo').toUpperCase();
+            //cb(res);
+            http.request(options, function(response){
+                var str = '';
+
+            //another chunk of data has been recieved, so append it to `str`
+            response.on('data', function (chunk) {
+            str += chunk;
+            });
+
+            //the whole response has been recieved, so we just print it out here
+            response.on('end', function () {
+                cb(str);
+            });
+            }).end();
         }
     });
     d.pipe(stream).pipe(d);
