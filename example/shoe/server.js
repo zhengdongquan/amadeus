079ab9ce1ecd9d204dfab40226a4dbdef18bd199
aaron <zhengdongquan@hotmail.com>
2015-11-26 14:52:13 -0500

    add async

--- example/shoe/server.js
+++ example/shoe/server.js
@@ -14,21 +14,72 @@ var options = {
 var sock = shoe(function (stream) {
     var d = dnode({
         transform : function (s, cb) {
-            //var res = s.replace(/[aeiou]{2,}/, 'oo').toUpperCase();
-            //cb(res);
+            
+
+
+
+
+
+
+            async.parallel([
+        //Load user
+        function(callback) {
             http.request(options, function(response){
                 var str = '';
 
-            //another chunk of data has been recieved, so append it to `str`
-            response.on('data', function (chunk) {
-            str += chunk;
-            });
+                //another chunk of data has been recieved, so append it to `str`
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
 
-            //the whole response has been recieved, so we just print it out here
-            response.on('end', function () {
-                cb(str);
-            });
+                //the whole response has been recieved, so we just print it out here
+                response.on('end', function () {
+                    cb(str);
+                });
             }).end();
+        },
+        //Load posts
+        function(callback) {
+            http.request("http://vacancesinorama.com/", function(response){
+                var str = '';
+
+                //another chunk of data has been recieved, so append it to `str`
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+
+                //the whole response has been recieved, so we just print it out here
+                response.on('end', function () {
+                    cb(str);
+                });
+            }).end();
+        }
+    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
+        if (err) return next(err); //If an error occured, we let express/connect handle it by calling the "next" function
+        //Here locals will be populated with 'user' and 'posts'
+        cb("do some thing wrong.");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
         }
     });
     d.pipe(stream).pipe(d);
