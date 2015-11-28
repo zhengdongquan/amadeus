19cdb453f21d34ad269d21d3e61cc9fd13fe1dde
aaron <zhengdongquan@hotmail.com>
2015-11-28 11:40:31 -0500

    dd

--- example/shoe/server.js
+++ example/shoe/server.js
@@ -5,6 +5,8 @@ var dnode = require('dnode');
 
 var async= require('async');
 
+var cheerio = require('cheerio');
+
 var server = http.createServer(ecstatic);
 server.listen(9999);
 
@@ -15,15 +17,15 @@ var options = {
 
 var options2 = {
   host: 'wftc1.e-travel.com',
-  path: 'plnext/AIEA00SA00S/TravelShopperAvailability.action?LANGUAGE=US&SITE=A00SA00S&AIR_CABIN=E&TRIP_TYPE=R&TRAVELLER_TYPE_1=ADT&TRAVELLER_TYPE_2=CHD&TRAVELLER_TYPE_3=&TRAVELLER_TYPE_4=&TRAVELLER_TYPE_5=&B_ANY_TIME_1=TRUE&B_ANY_TIME_2=TRUE&B_DATE_1=201603030000&B_DATE_2=201605100000&PRODUCT_TYPE_1=STANDARD_AIR&DISTANCE_UNIT=M&B_TIME_WINDOW_1=&B_TIME_WINDOW_2=&MINUS_DATE_RANGE_1=&MINUS_DATE_RANGE_2=&PLUS_DATE_RANGE_1=&PLUS_DATE_RANGE_2=&PLTG_FROMPAGE=MPSEARCH&TRIP_FLOW=YES&B_CAL_DATE_1=&B_CAL_DATE_2=&SEARCH_PAGE=MP&AIR_MAX_CONNECTIONS=4&B_DAY=10&B_LOCATION_1=YUM&B_TIME_TO_PROCESS=ANY&E_DAY=10&E_LOCATION_1=PEK&E_TIME_TO_PROCESS=ANY&departuredateInput=03%2F03%2F2016&returndateInput=05%2F10%2F2016&FIELD_ADT_NUMBER=1&FIELD_CHD_NUMBER=0'
+  path: '/plnext/AIEA00SA00S/TravelShopperAvailability.action?LANGUAGE=US&SITE=A00SA00S&AIR_CABIN=E&TRIP_TYPE=R&TRAVELLER_TYPE_1=ADT&TRAVELLER_TYPE_2=CHD&TRAVELLER_TYPE_3=&TRAVELLER_TYPE_4=&TRAVELLER_TYPE_5=&B_ANY_TIME_1=TRUE&B_ANY_TIME_2=TRUE&B_DATE_1=201603030000&B_DATE_2=201605100000&PRODUCT_TYPE_1=STANDARD_AIR&DISTANCE_UNIT=M&B_TIME_WINDOW_1=&B_TIME_WINDOW_2=&MINUS_DATE_RANGE_1=&MINUS_DATE_RANGE_2=&PLUS_DATE_RANGE_1=&PLUS_DATE_RANGE_2=&PLTG_FROMPAGE=MPSEARCH&TRIP_FLOW=YES&B_CAL_DATE_1=&B_CAL_DATE_2=&SEARCH_PAGE=MP&AIR_MAX_CONNECTIONS=4&B_DAY=10&B_LOCATION_1=YUM&B_TIME_TO_PROCESS=ANY&E_DAY=10&E_LOCATION_1=PEK&E_TIME_TO_PROCESS=ANY&departuredateInput=03%2F03%2F2016&returndateInput=05%2F10%2F2016&FIELD_ADT_NUMBER=1&FIELD_CHD_NUMBER=0'
 };
 
 
 var sock = shoe(function (stream) {
     var d = dnode({
-        transform : function (s, cb) {
+        transform: function (s, cb) {
 
-        http.request(options, function(response){
+            http.request(options2, function (response) {
                 var str = '';
 
                 //another chunk of data has been recieved, so append it to `str`
@@ -33,11 +35,12 @@ var sock = shoe(function (stream) {
 
                 //the whole response has been recieved, so we just print it out here
                 response.on('end', function () {
-                    cb(str);
+                    $ = cheerio.load(str);
+                    cb($('div #scriptToRemove').text());
                 });
             }).end();
 
-            
+
         }
     });
     d.pipe(stream).pipe(d);
