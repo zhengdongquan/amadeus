d2c2d45ea73980c4a0fb6c1fb95f7eb3025b28ff
aaron <zhengdongquan@hotmail.com>
2015-11-28 11:28:14 -0500

    query amadeus

--- example/shoe/server.js
+++ example/shoe/server.js
@@ -14,8 +14,8 @@ var options = {
 };
 
 var options2 = {
-  host: 'vacancesinorama.com',
-  path: 'tourpage/index.php?id=index_montreal_fr'
+  host: 'wftc1.e-travel.com',
+  path: 'plnext/AIEA00SA00S/TravelShopperAvailability.action?LANGUAGE=US&SITE=A00SA00S&AIR_CABIN=E&TRIP_TYPE=R&TRAVELLER_TYPE_1=ADT&TRAVELLER_TYPE_2=CHD&TRAVELLER_TYPE_3=&TRAVELLER_TYPE_4=&TRAVELLER_TYPE_5=&B_ANY_TIME_1=TRUE&B_ANY_TIME_2=TRUE&B_DATE_1=201603030000&B_DATE_2=201605100000&PRODUCT_TYPE_1=STANDARD_AIR&DISTANCE_UNIT=M&B_TIME_WINDOW_1=&B_TIME_WINDOW_2=&MINUS_DATE_RANGE_1=&MINUS_DATE_RANGE_2=&PLUS_DATE_RANGE_1=&PLUS_DATE_RANGE_2=&PLTG_FROMPAGE=MPSEARCH&TRIP_FLOW=YES&B_CAL_DATE_1=&B_CAL_DATE_2=&SEARCH_PAGE=MP&AIR_MAX_CONNECTIONS=4&B_DAY=10&B_LOCATION_1=YUM&B_TIME_TO_PROCESS=ANY&E_DAY=10&E_LOCATION_1=PEK&E_TIME_TO_PROCESS=ANY&departuredateInput=03%2F03%2F2016&returndateInput=05%2F10%2F2016&FIELD_ADT_NUMBER=1&FIELD_CHD_NUMBER=0'
 };
 
 
@@ -23,10 +23,7 @@ var sock = shoe(function (stream) {
     var d = dnode({
         transform : function (s, cb) {
 
-        async.parallel([
-        //Load user
-        function(callback) {
-            http.request(options, function(response){
+        http.request(options, function(response){
                 var str = '';
 
                 //another chunk of data has been recieved, so append it to `str`
@@ -39,47 +36,6 @@ var sock = shoe(function (stream) {
                     cb(str);
                 });
             }).end();
-        },
-        //Load posts
-        function(callback) {
-            http.request(options2, function(response){
-                var str = '';
-
-                //another chunk of data has been recieved, so append it to `str`
-                response.on('data', function (chunk) {
-                    str += chunk;
-                });
-
-                //the whole response has been recieved, so we just print it out here
-                response.on('end', function () {
-                    cb(str);
-                });
-            }).end();
-        }
-    ], function(err) { //This function gets called after the two tasks have called their "task callbacks"
-        if (err) return next(err); //If an error occured, we let express/connect handle it by calling the "next" function
-        //Here locals will be populated with 'user' and 'posts'
-        cb("do some thing wrong.");
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
             
         }
