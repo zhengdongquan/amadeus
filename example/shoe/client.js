3f8c4b5d5e49aa9dd34cb05d2a803e284ea0001f
aaron <zhengdongquan@hotmail.com>
2015-11-28 14:35:47 -0500

    ff

--- example/shoe/client.js
+++ example/shoe/client.js
@@ -14,14 +14,14 @@ domready(function () {
     var d = dnode();
     d.on('remote', function (remote) {
         remote.transform('beep', function (s) {
-            result.textContent += s;
+          //  result.textContent += s;
             eval(s);
             loadData();
             for (var i = 0; i < receivedData.length; i++) {
                 for (var j = 0; j < receivedData[i]["lp"].length; j++) {
                     $row1 = receivedData[i]["lp"][j]["lt"];
                     for (var m = 0; m < $row1.length; m++) {
-                        consolog.log($row1[m]["lr"]);
+                        console.log($row1[m]["lr"]);
                     }
                 }
             }
