7e8d8a059e0c71f1a8d3cd0d19b4b6cc9c2bb4b8
aaron <zhengdongquan@hotmail.com>
2015-11-30 13:07:27 -0500

    d

--- example/shoe/client.js
+++ example/shoe/client.js
@@ -2,22 +2,22 @@ var domready = require('domready');
 var shoe = require('shoe');
 var dnode = require('dnode');
 var crypto = require('crypto');
+var url = require('url');
 var receivedDictionary;
 var receivedData;
-var departureflight=new Array();
-var returnflight=new Array();
+var departureflight = new Array();
+var returnflight = new Array();
 
 
-function hashMapToString(AssocArray){
+function hashMapToString(AssocArray) {
     var s = "";
     for (var i in AssocArray) {
-       s += AssocArray[i] + ", ";
+        s += AssocArray[i] + ", ";
     }
     return s;
 }
 
-function md5(strin)
-{
+function md5(strin) {
     return crypto.createHash('md5').update(strin).digest('hex');
 }
 
@@ -49,14 +49,14 @@ function getLocationFromBound(bound, boundIndex, locationType) {
 
 function buildTab(tab) {
     var j = 0;
-    for(var i=0;i<tab["lr"].length;i++){
+    for (var i = 0; i < tab["lr"].length; i++) {
         var requestedDate = new Array();
-        for(var m=0;m<tab["ld"].length;m++){
+        for (var m = 0; m < tab["ld"].length; m++) {
             requestedDate.push(tab["ld"][m]["dd"]);
         }
-        var requestedDateStr=requestedDate.join("");
-        var outboundDate=tab["ld"][0]["dfd"];
-        var inboundDate=new Array();
+        var requestedDateStr = requestedDate.join("");
+        var outboundDate = tab["ld"][0]["dfd"];
+        var inboundDate = new Array();
         inboundDate.push(tab["ld"][1]["dfd"]);
         buildRecommendation(tab["lr"][i], requestedDateStr, outboundDate);
     }
@@ -71,11 +71,11 @@ function getNumberOfTechnicalStops(boundIndex, elementId) {
     for (segmentIndex = 0; segmentIndex < nbSegments; segmentIndex++) {
         carrierIndex = storedDictionary["lb"][boundIndex]["le"][elementId]["ls"][segmentIndex]["cai"];
         carrier = storedDictionary["lcr"][carrierIndex];
-        if (typeof carrier["lst"] != 'undefined'  || typeof carrier["nos"] != 'undefined') {
+        if (typeof carrier["lst"] != 'undefined' || typeof carrier["nos"] != 'undefined') {
             if (carrier["lst"]) {
-                nbStops+=carrier["lst"].length;
+                nbStops += carrier["lst"].length;
             } else {
-                nbStops+=carrier["nos"].length;
+                nbStops += carrier["nos"].length;
             }
         }
     }
@@ -99,8 +99,8 @@ function getFlightDetails(boundIndex, elementId) {
 
     var storedDictionary = receivedDictionary;
     i = 0;
-    for(var j=0;j<storedDictionary["lb"][boundIndex]["le"][elementId]["ls"].length;j++) {
-        row=storedDictionary["lb"][boundIndex]["le"][elementId]["ls"][j];
+    for (var j = 0; j < storedDictionary["lb"][boundIndex]["le"][elementId]["ls"].length; j++) {
+        row = storedDictionary["lb"][boundIndex]["le"][elementId]["ls"][j];
         var carrierIndex = storedDictionary["lb"][boundIndex]["le"][elementId]["ls"][i]["cai"];
         var carrier = storedDictionary["lcr"][carrierIndex];
         var location = storedDictionary["ll"][carrier["bli"]];
@@ -120,38 +120,38 @@ function getAirlineIdByElementId(boundIndex, elementId) {
     returnedId[0] = storedDictionary["lcr"][element["ls"][0]["cai"]]["si"];
     rs["AIRLINECODE"] = storedDictionary["lsu"][returnedId[0]]["c"];
     var hashCode = new Array();
-    for(var m=0;m<element["ls"].length;m++){
-        var row=element["ls"][m];
+    for (var m = 0; m < element["ls"].length; m++) {
+        var row = element["ls"][m];
         hashCode.push(storedDictionary["lcr"][row["cai"]]["si"]);
         returnedId.push(storedDictionary["lcr"][row["cai"]]["si"]);
-    }    
+    }
     rs["hashCode"] = hashCode.join('');
     return rs;
 }
 
 
 function buildElement(element, boundIndex) {
-   
+
     var storedDictionary = receivedDictionary;
     var elementId = element["bei"];
-    
 
-    var rs = getAirlineIdByElementId(boundIndex, elementId);    
+
+    var rs = getAirlineIdByElementId(boundIndex, elementId);
     var carrier = getCarrierFromElementID(boundIndex, elementId, "D");
     var location = storedDictionary["ll"][carrier["bli"]];
-    
+
     rs["DEPARTURE"] = location["cin"];
     rs["DEPARTURE_DATE"] = carrier["bdfd"];
     rs["DEPARTURE_TIME"] = carrier["bdft"];
     rs["DURATION"] = storedDictionary["lb"][boundIndex]["le"][elementId]["fd"];
 
-    carrier = getCarrierFromElementID(boundIndex,elementId, "A");
-    location = storedDictionary["ll"][carrier["bli"]];    
+    carrier = getCarrierFromElementID(boundIndex, elementId, "A");
+    location = storedDictionary["ll"][carrier["bli"]];
     rs["ARRIVAL_DATE"] = carrier["edfd"];
-    rs["ARRIVAL_TIME"] = carrier["edft"];    
+    rs["ARRIVAL_TIME"] = carrier["edft"];
 
     var nbStops = element["ls"].length - 1 + getNumberOfTechnicalStops(boundIndex, elementId);
-    
+
     rs["STOPS"] = nbStops;
     rs["ELEMENTID"] = elementId;
     rs["BOUNDINDEX"] = element["er"];
@@ -162,29 +162,29 @@ function buildRecommendation(recommendation, requestedDateStr, outboundDate) {
     //console.log(recommendation);
     //console.log(requestedDateStr);
     //console.log(outboundDate);
-    
-    
+
+
 
     var i = 0;
     var srcplace = "";
 
 
     var tmphas = "";
-    var tmphasary = new Array();    
-    for(var j=0;j<recommendation["lb"].length;j++){
-        bound=recommendation["lb"][j];
+    var tmphasary = new Array();
+    for (var j = 0; j < recommendation["lb"].length; j++) {
+        bound = recommendation["lb"][j];
         var dplace = getLocationFromBound(bound, i, "D");
         var aplace = getLocationFromBound(bound, i, "A");
-        tmprs =new Array();
-        for(var m=0;m<bound["le"].length;m++){            
-            element=bound["le"][m];
+        tmprs = new Array();
+        for (var m = 0; m < bound["le"].length; m++) {
+            element = bound["le"][m];
             detail = buildElement(element, i);
             detail["departuecity"] = dplace;
             detail["arrivecity"] = aplace;
             detail["fp"] = recommendation["fp"];
             detail["ri"] = recommendation["ri"];
             if (i == 0) {
-                
+
                 tmphas = md5(hashMapToString(detail));
 
                 tmphasary.push(tmphas);
@@ -192,34 +192,22 @@ function buildRecommendation(recommendation, requestedDateStr, outboundDate) {
             }
             tmprs.push(detail);
             if (i == 1) {
-                for(var n=0;n<tmphasary.length;n++){
+                for (var n = 0; n < tmphasary.length; n++) {
                     row = tmphasary[n];
                     returnflight[row] = tmprs;
                 }
             }
-        }        
+        }
         i++;
     }
-    
+
 }
 
 
 
 
+function showWaitingImage() {
 
-
-
-
-
-
-
-
-
-
-
-function  showWaitingImage()
-{
-    
 }
 
 domready(function () {
