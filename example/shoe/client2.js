f1cb5c73b4fcd6fe13e3acec78e4601df487f29b
aaron <zhengdongquan@hotmail.com>
2015-11-30 14:16:17 -0500

    h

--- example/shoe/client2.js
+++ example/shoe/client2.js
@@ -3,7 +3,7 @@
 var websocketStream = require('websocket-stream');
 var dnode = require('dnode');
 
-var c = websocketStream('ws://localhost:9999');
+var c = websocketStream('ws://52.33.184.12:9999');
 var d = dnode()
     .on('remote', function(remote)
     {
